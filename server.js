0eae093675dabbc7fa4b9ec8c377507132dd9771
evertonoliveira18973-wq <evertonoliveira18973@gmail.com>
2026-07-22 21:04:39 -0300

    Refactor server.js to include homepage route

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const cors = require("cors");
 const OpenAI = require("openai");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("."));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 const client = new OpenAI({
     apiKey: process.env.AZURE_OPENAI_API_KEY,
